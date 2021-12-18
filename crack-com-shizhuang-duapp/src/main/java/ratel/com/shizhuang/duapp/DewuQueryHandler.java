7a76c9e8f7b2fb4cf0a5187ecd3aabd2fca799d6
liguobao <codelover@qq.com>
2021-12-18 16:04:10 +0800

    完成提篮桥一日游初版

--- crack-com-shizhuang-duapp/src/main/java/ratel/com/shizhuang/duapp/DewuQueryHandler.java
+++ crack-com-shizhuang-duapp/src/main/java/ratel/com/shizhuang/duapp/DewuQueryHandler.java
@@ -2,7 +2,9 @@ package ratel.com.shizhuang.duapp;
 
 import android.util.Log;
 
+import com.virjar.ratel.api.RatelToolKit;
 import com.virjar.ratel.api.inspect.ClassLoadMonitor;
+import com.virjar.ratel.api.rposed.RC_MethodHook;
 import com.virjar.ratel.api.rposed.RposedHelpers;
 import com.virjar.sekiro.api.ActionHandler;
 import com.virjar.sekiro.api.SekiroRequest;
@@ -12,6 +14,8 @@ import com.virjar.sekiro.api.databind.AutoBind;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+
+import external.com.alibaba.fastjson.JSON;
 
 public class DewuQueryHandler implements ActionHandler {
     @Override
@@ -26,30 +30,53 @@ public class DewuQueryHandler implements ActionHandler {
     @AutoBind
     private String body;
 
+    static {
+        // 这里是为了在MyACallback onSuccess的时候，把数据通过SekiroResponse回传给服务端
+        // 理论上在MyACallback上面做更舒服，但是MyACallback的代码是smali，太难写了，还是直接使用hook比较舒服
+        RposedHelpers.findAndHookMethod("com.ratel.MyDataModel",
+                RatelToolKit.hostClassLoader, "onSuccess",
+                Object.class,
+                new RC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        SekiroResponse networkCallback = RposedHelpers.getObjectField(
+                                param.thisObject,"myACallbackResult");
+                        Log.i(CommonConst.AppTag, "com.ratel.MyDataModel onSuccess,param.args[0]:" + param.args[0].toString());
+                        Object dataResult = param.args[0];
+                        Object searchWord = RposedHelpers.callMethod(dataResult,"getSearchWord");
+                        Log.i(CommonConst.AppTag,"searchWord:" + searchWord.toString());
+                        Object listData = RposedHelpers.callMethod(dataResult,"getList");
+                        if(networkCallback !=null){
+                            networkCallback.success(listData);
+                        }
+                    }
+                });
+    }
+
     @Override
     public void handleRequest(SekiroRequest sekiroRequest, final SekiroResponse sekiroResponse) {
         String searchJavaName = "com.shizhuang.duapp.modules.search.facade.SearchFacade";
         Class<?> searchJava = ClassLoadMonitor.tryLoadClass(searchJavaName);
         if (searchJava != null) {
-            Method[] methods =  searchJava.getMethods();
-            Class<?> viewHandlerClass = ClassLoadMonitor.tryLoadClass(
-                    "com.shizhuang.duapp.common.helper.net.facade.ViewHandler");
-            Class<?> hotWordModelClass = ClassLoadMonitor.tryLoadClass(
-                    "com.shizhuang.duapp.modules.search.model.HotWordModel");
-            if(viewHandlerClass!=null) {
-                Log.i(CommonConst.AppTag, viewHandlerClass.toString());
+            Class<?> myDataModel = ClassLoadMonitor.tryLoadClass(
+                    "com.ratel.MyDataModel");
+            if(myDataModel!=null) {
+                Log.i(CommonConst.AppTag, myDataModel.toString());
                 try{
-                    Method l = searchJava.getMethod("l");
-                    Class<?>[] parameterTypes = l.getParameterTypes();
-                    sekiroResponse.success(parameterTypes);
+                    Object myDataModelProxy = RposedHelpers.newInstance(myDataModel, sekiroResponse);
+                    RposedHelpers.callStaticMethod(searchJava, "l", myDataModelProxy);
+                    Log.i(CommonConst.AppTag,"callStaticMethod finish");
+
                 }catch (Exception ex){
-                    Log.e(CommonConst.AppTag, "get mModel fail", ex);
-                    sekiroResponse.success(methods);
+                    Log.e(CommonConst.AppTag, "callStaticMethod fail", ex);
                 }
+            }else {
+                Log.i(CommonConst.AppTag, "com.ratel.MyDataModel not found.");
+
             }
+            // sekiroResponse.success(searchJava.getMethods());
 
         }
-        return;
-
     }
 }
